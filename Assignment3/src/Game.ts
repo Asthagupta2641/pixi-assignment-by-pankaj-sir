9d20cae534558b275cf37a27f2b0dcd37fc20bf6
Astha Gupta <asgupta@ingenuitygaming.com>
2022-12-30 15:41:12 +0530

    commit

--- Assignment3/src/Game.ts
+++ Assignment3/src/Game.ts
@@ -7,17 +7,17 @@ export class Game extends Application {
     super(opt);
     this.nextbutton();
     this.previousbutton();
-    this.image1();
+    this.image2();
     // this.button2();
     // this.image();
     // this.onclick();
   }
 
   public nextbutton(){
-     const sprite= Sprite.from("./assets/next button.png");
+     const sprite= Sprite.from("./assets/ne.png");
      //sprite.anchor.set(0.1);
     sprite.width=innerWidth/11;
-    sprite.height=innerHeight/11;
+    sprite.height=innerHeight/8;
     sprite.interactive = true;
     sprite.buttonMode=true;
     sprite.x=1230;
@@ -31,16 +31,28 @@ export class Game extends Application {
     
   }
   public previousbutton(){
-    const sprite= Sprite.from("./assets/previous button.png");
+    const sprite= Sprite.from("./assets/pr.png");
     // sprite.anchor.set(0.1);
     sprite.width=innerWidth/11;
-    sprite.height=innerHeight/11;
+    sprite.height=innerHeight/8;
     sprite.x=3;
     sprite.y=250;
     sprite.interactive = true;
     sprite.buttonMode=true;
     this.stage.addChild(sprite);
     sprite.on("pointerdown",this.onhide.bind(this))
+  }
+  public image2(){
+    const sprite= Sprite.from("./assets/lo.png");
+    sprite.anchor.set(0.1);
+    sprite.width=innerWidth/2.5;
+    sprite.height=innerHeight/1.5;
+    sprite.x=450;
+    sprite.y=150;
+    sprite.interactive = true;
+   //sprite.cursor = 'pointer';
+    this.stage.addChild(sprite)
+
   }
   public image1(){
     const sprite= Sprite.from("./assets/gaming-background.jpg");
@@ -54,18 +66,7 @@ export class Game extends Application {
     this.stage.addChild(sprite)
 
   }
-  public image2(){
-    const sprite= Sprite.from("./assets/loading image.png");
-    // sprite.anchor.set(0.1);
-    sprite.width=innerWidth/1.4;
-    sprite.height=innerHeight/1.5;
-    sprite.x=170;
-    sprite.y=100;
-    sprite.interactive = true;
-   //sprite.cursor = 'pointer';
-    this.stage.addChild(sprite)
-
-  }
+  
   public onclick(){
     this.stage.removeChildren();
     this.previousbutton();
